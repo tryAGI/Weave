3f52092cefd744ca464da227fdc3514d0197f72f
Konstantin S. <havendv@gmail.com>
2026-05-24 07:24:56 +0400

    feat: Updated OpenAPI spec (#36)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Weave/Generated/Weave.ServiceClient.ReadRootHealthGet.g.cs
+++ src/libs/Weave/Generated/Weave.ServiceClient.ReadRootHealthGet.g.cs
@@ -345,17 +345,15 @@ namespace Weave
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::Weave.ApiException(
+                                    throw global::Weave.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -390,17 +388,15 @@ namespace Weave
                                     {
                                     }
 
-                                    throw new global::Weave.ApiException(
+                                    throw global::Weave.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
